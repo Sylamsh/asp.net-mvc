8f27e8b31b727fb765f58e0147d455f11d1836dc
Sylamsh <amrutasrisvara@gmail.com>
2022-04-29 00:43:29 +0530

    Add project files.

---
+++ Data/Migrations/20220428181758_intialSetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Quotes.Data.Migrations
+{
+    public partial class intialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quote",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quote", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Quote");
+        }
+    }
+}
---
+++ Models/Quote.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quotes.Data
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string Author { get; set; }
+
+        public Quote() { 
+        
+        }
+
+
+    }
+}
